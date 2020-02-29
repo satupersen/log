a0f971e9f753fa393667b1783b193dd45e3e82ef
satupersen <alfiyanmail24@gmail.com>
2020-02-29 17:57:09 +0700

    first commit

---
+++ LogServiceProvider.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Satupersen\Log;
+
+use Satupersen\Support\ServiceProvider;
+
+class LogServiceProvider extends ServiceProvider
+{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton('log', function ($app) {
+            return new LogManager($app);
+        });
+    }
+}
